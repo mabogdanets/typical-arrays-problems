4a96d9dbedb3fc6ebbe1fc0f4370b41bc57bf8e5
mabogdanets <maria.bogdanets@gmail.com>
2021-03-03 16:00:20 +0300

    feat: min, max, avg func

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
 
 exports.min = function min (array) {
-  return 0;
+  function compareNumeric(a, b) {
+    if (a > b) return 1; 
+    if (a == b) return 0;
+    if (a < b) return -1; 
+  } 
+  if( (typeof array == "undefined")|| (array.length==0)){
+    return 0;
+  }else{
+    array.sort(compareNumeric);
+    return array[0];
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  function compareNumeric(a, b) {
+    if (a > b) return 1; 
+    if (a == b) return 0;
+    if (a < b) return -1; 
+  } 
+  if( (typeof array == "undefined" ) || (array.length==0)){
+    return 0;
+  }else{
+    
+    array.sort(compareNumeric);
+    return array[array.length-1];
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+ 
+  if( (typeof array == "undefined") || (array.length==0) ){
+    return 0;
+  }else{
+  let acc=0;
+  for(let i=0; i<array.length; i++){
+    acc = acc + array[i];
+  }
+  let out=acc/array.length;
+  return out;
+}
 }
